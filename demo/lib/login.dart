e90e46b448def62a6a61c10f4540b2a1563d2d76
FURYS <mainguyenthanhdat10@gmail.com>
2022-04-07 11:44:43 +0700

    lan2

--- demo/lib/login.dart
+++ demo/lib/login.dart
@@ -16,7 +16,7 @@ class login extends StatelessWidget {
     var CommunityMaterialIcons;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "NHU kien ngu",
+      title: "Login",
       home: Scaffold(
           appBar: AppBar(
               backgroundColor: Colors.white,
